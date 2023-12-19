9f8049fc4eb0f871fc564171e91652565f858b70
elmacan <betozsan@hotmail.com>
2023-12-20 02:38:47 +0300

    control the code

--- Test.java
+++ Test.java
@@ -2,23 +2,39 @@ public class Test{
     public static void main(String[] args){
         GameDeck gd=new GameDeck();
         Player p=new Player();
+        Player p2=new Player();
 
-        //gd.shuffleGameDeck();
+
         System.out.println("KARILMAMIŞ GAME DECK");
         gd.printGameDeck();
         System.out.println("----------------------------------");
+        System.out.println("KARILMIŞ GAME DECK");
+        gd.shuffleGameDeck();
+        gd.printGameDeck();
+        System.out.println("----------------------------------");
         p.createPlayerCards();
-        p.fromGameDeckForCpu(gd);
+        p.fromGameDeckForCpu(gd);  //
         System.out.println("before shuffle CPU PLAYER DECK");
-
         p.printPlayerDeck();
         System.out.println("----------------------------------");
         System.out.println("after shuffle CPU PLAYER DECK");
-          p.shufflePlayerDecks();
-          p.printPlayerDeck();
+        p.shufflePlayerDecks();
+        p.printPlayerDeck();
         System.out.println("----------------------------------");
-        System.out.println("PLAYER HANDS");
-        p.pickForPlayerHands();
+        System.out.println("CPU HANDS");
+        p.pickForPlayerHands();//içinde yazdırıyor
+        System.out.println("----------------------------------");
+        p2.createPlayerCards();
+        p2.fromGameDeckForHuman(gd);
+        System.out.println("before shuffle HUMAN PLAYER DECK");
+        p2.printPlayerDeck();
+        System.out.println("-----------------------------------");
+        System.out.println("after shuffle HUMAN PLAYER DECK");
+        p2.shufflePlayerDecks();
+        p2.printPlayerDeck();
+        System.out.println("-----------------------------------");
+        p2.pickForPlayerHands();
+
 
     }
 }
